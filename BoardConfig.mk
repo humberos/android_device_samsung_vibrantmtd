9867f826f39f1a01ec75321d69d394fb25a6fb2c
Humberto Borba <humberos@gmail.com>
2015-02-26 14:30:05 -0300

    vibrantmtd: first commit
    Signed-off-by: Humberto Borba <humberos@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2013 OmniROM Project
+# Copyright (C) 2007 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# BoardConfig.mk
+#
+# Product-specific compile-time definitions.
+#
+
+# Telephony
+BOARD_USES_FROYO_RILCLIENT := true
+
+# Kernel Config
+TARGET_KERNEL_CONFIG := omni_vibrantmtd_defconfig
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/vibrantmtd/bluetooth
+
+# Recovery
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/vibrantmtd/recovery/recovery_keys.c
+
+TARGET_OTA_ASSERT_DEVICE := vibrant,vibrantmtd,SGH-T959
+
+# Import the aries-common BoardConfigCommon.mk
+include device/samsung/aries-common/BoardConfigCommon.mk
---
+++ libgps/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := gps.$(TARGET_BOOTLOADER_BOARD_NAME)
+LOCAL_SHARED_LIBRARIES := liblog libdl libcrypto
+LOCAL_C_INCLUDES += external/openssl/include
+LOCAL_SRC_FILES := gps.c
+LOCAL_CFLAGS += -fno-short-enums
+#LOCAL_CFLAGS += -DNEEDS_INITIAL_XTRA
+LOCAL_PRELINK_MODULE := false
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+
+include $(BUILD_SHARED_LIBRARY)
